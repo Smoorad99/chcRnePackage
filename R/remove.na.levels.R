f032aa881b6cffd0af1e67ddc6ffad17d433c218
Saul <slmooradian@csuchico.edu>
2024-04-02 09:00:32 -0700

    documentation

--- R/remove.na.levels.R
+++ R/remove.na.levels.R
@@ -1,14 +1,9 @@
 #' Remove NA Levels in Ordered Factors
 #'
-#' This function explicitly marks NA levels in a factor as `NA`, without removing them.
-#' It is useful for cleaning up factors by ensuring that NA levels are properly recognized,
-#' especially before conducting further data analysis or modeling.
 #'
 #' @param x A factor variable containing
 #'
 #' @return The same factor variable with NA levels explicitly marked as `NA`.
-#'         No levels are actually removed from the factor; instead, they are
-#'         just marked to ensure correct recognition and handling in R.
 #'
 #' @examples
 #' # Given a factor with NA as one of its levels
@@ -19,5 +14,5 @@ remove.na.levels <- function(x){
   return(x)
 }
 # # list out all variable names with this problem and apply function
-# cols.with.na.lvls <- pisaitems |> dplyr::select(where(is.ordered)) |> names()
-# pisaitems <- pisaitems |> dplyr::mutate(across(all_of(cols.with.na.lvls), ~remove.na.levels(.x)))
+
+# example using dplyr::across to show how to apply function across multiple levels
