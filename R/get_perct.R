79cf94fa02a6586889961534dfe29b5111acbe9c
Saul <slmooradian@csuchico.edu>
2024-04-01 18:27:20 -0700

    stash attempt

--- R/get_perct.R
+++ R/get_perct.R
@@ -4,8 +4,8 @@
 ## Get percent for variable for given value (ex: Housing - Sleeping Places)
 get_perct <- function(x, value) {
   paste0(sum(x==value, na.rm=TRUE), "/", sum(!is.na(x)),
-         " (", percent(mean(x==value, na.rm = TRUE), accuracy=.1), ")")
+         " (", formattable::percent(mean(x==value, na.rm = TRUE), digits = 1), ")")
 }
 
 
-get_perct(data[[question]], values[idx])
+get_perct(bns2_pkg_data$q14_17, "Yes")
