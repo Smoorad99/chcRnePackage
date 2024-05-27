88aa75ba25192f5d98ac7751ee5b9f51e4675f1b
Saul <slmooradian@csuchico.edu>
2024-05-27 13:10:27 -0700

    line by line summary for pnta function

--- R/pnta_unanswered_to_miss.R
+++ R/pnta_unanswered_to_miss.R
@@ -9,7 +9,7 @@
 #' This parameter can be a prefix that all the columns of interest contain, or a list of the specific columns you want converted.
 #' @param pnta The variable that contains the "Prefer not to answer" option.
 #' @param prefix If prefix = TRUE you can use a prefix to select columns, if prefix = FALSE
-#' you must select columns by name.
+#' you must select columns by name. The function defaults to prefix = FALSE
 #'
 #' @return A modified data frame with NA assigned to cells in specified rows where prefer not to answer was selected.
 #' @export
@@ -45,17 +45,22 @@
 
 
 pnta.unanswered.to.miss <- function(data, these.cols, pnta, prefix = TRUE){
+  # If the user sets prefix to TRUE, get all columns with the prefix in their name.
   if (prefix) {
     these.cols <- grepl(these.cols, names(data))
   }
+  # If there are NAs in the these.cols, sum the number of non-NA values in each row of these.cols
   if (anyNA(data[, these.cols])) {
     n.answer <- rowSums(!is.na(data[,these.cols]))
   }
+  # If there aren't NAs, sum each row in these.cols
   if (!anyNA(data[, these.cols])) {
-    n.answer <- rowSums(data[,these.cols])   # count number of response per row
+    n.answer <- rowSums(data[,these.cols])
   }
-  n.answer <- replace(n.answer, pnta == 1 | pnta == "Prefer not to answer", 0)  # if prefer not to answer (PNTA) is marked, set # answers to 0
-  data[n.answer == 0, these.cols] <- NA         # if #answers is 0, set all to NA missing.
+  # if prefer not to answer (PNTA) is marked, set # answers to 0
+  n.answer <- replace(n.answer, pnta == 1 | pnta == "Prefer not to answer", 0)
+  # if #answers is 0, set all to NA missing.
+  data[n.answer == 0, these.cols] <- NA
   return(data)
 }
 
