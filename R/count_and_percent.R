006ad4c448b57676e088035302d37056c283c645
Saul <slmooradian@csuchico.edu>
2024-06-28 14:09:36 -0700

    new and improved count and percent

--- R/count_and_percent.R
+++ R/count_and_percent.R
@@ -1,14 +1,13 @@
-
-
-## Get the count and percent for n (...) categories
+## Get the count and percent for n categories
 #' Creates a string with count and percent
 #' @description
 #' This function calculates counts and percentages for specified categories within a variable
 #' then returns the total count and total percent of those categories relative to the
 #' variable they reside in.
 #'
+#' @param df The data frame of interest.
 #' @param var A variable for which the counts and percents are calculated.
-#' @param ... One or more categories of `var` to get the counts and calculate the percentages. If the ellipsis is left blank, the function
+#' @param values One or more categories of `var` to get the counts and calculate the percentages. If left blank, the function
 #' will calculate the count and percent for the category with highest count.
 #' @param format1 Allows for two different formats of the count and percent. Defaults to `TRUE` which outputs the
 #' the count then the percent in parentheses "count (percent)". If `format1` is set to `FALSE` the function outputs
@@ -18,26 +17,27 @@
 #' @export
 #'
 #' @examples
-#'count_and_percent(var = bns2_pkg_data$q14_1, "Yes")
+#' count_and_percent(df = bns2_pkg_data, var = q13, values = c("Less than high school", "Some college"))
 #'
-count_and_percent <-  function(var, ..., format1 = TRUE) {  # Takes in a variable and an ellipsis ('...') which represents multiple categories
-  values <- list(...)  # Stores the values of each of the selected categories as a list
-  tblcount <- table(var)
-  name_common <- names(which.max(tblcount)) # Getting the category with the most responses
-  if (length(values) == 0) {
-    values <- name_common
-  } else {
-    values <- values
+#' # Using a pipe and leaving excluding `values` argument (defaults to the level of var with ,ost rows/responses)
+#' bns2_pkg_data |> count_and_percent(var = q13)
+#'
+#' @import dplyr
+#'
+count_and_percent <- function(df, var, values = NULL, format1 = TRUE) {
+  col <- df |>
+    dplyr::select({{var}})
+  if (is.null(values)) {
+    values <- names(which.max(table(col))) # Getting the category with the most responses when values are not provided (setting up default)
   }
-  counts <- lapply(values, function(val) sum(var == val, na.rm = TRUE))  # Applies the function to each element of the list
-  total <- sum(!is.na(var)) # Calculates the total count of non-missing values
-  percent <- sapply(counts, function(count) formattable::percent(count/total, digits = 5))
-  total_count <- sum(unlist(counts))
-  total_percent <- formattable::percent(total_count/total, digits = 1)
-  paste0(total_count, " (", total_percent, ")")
-
-  if (format1) return(paste0(total_count, " (", total_percent, ")"))
-
-  if (!format1) return(paste0("(n=", total_count, ", ", total_percent, ")"))
+  counts <- col |>
+    dplyr::filter({{var}} %in% {{values}}) |>
+    nrow() # Getting counts of the number of occurrences of values in var
+  total <- df |>
+    dplyr::filter(!is.na({{var}})) |>
+    nrow() # Getting count of non-NA rows in var
+  percent <- formattable::percent(counts/total, digits = 1)
+  if (format1) return(paste0(counts, " (", percent, ")"))
+  if (!format1) return(paste0("(n=", counts, ", ", percent, ")"))
 }
 
