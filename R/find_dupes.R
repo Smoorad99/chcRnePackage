ad3b1de0e248d42f45e3d150e94e9d6870a27fb5
Saul <slmooradian@csuchico.edu>
2024-07-26 13:21:19 -0700

    find_dupes function and remove levels vignette

--- R/find_dupes.R
+++ R/find_dupes.R
@@ -1,23 +1,41 @@
-#library(dplyr)
-#data <- datasets::sleep %>% rbind(c(.4, 1, 1)) %>% filter(extra != 3.7)
-
+library(dplyr)
+data <- datasets::sleep %>% rbind(c(.4, 1, 1)) %>% filter(extra != 3.7)
 
 find_dupes <- function (data, vars) { # Finding IDs with no matches
-  count <- data %>% group_by(across({{vars}})) %>%
-    tally() %>% ungroup() # Getting number of rows for each ID
-  if(length(vars) == 1 ) {
-    mult.dupes <- count %>% filter(n > 2) # If you only group by ID
-    matched <- count %>% filter(n == 2)
-    missing.response <- count %>% filter(n == 1)
-  }
-  else if(length(cols == 2)) {
-    mult.dupes <- count %>% filter(n > 1) # If you group by ID and (pre/post)
-    #matched <- count %>% group_by({{vars[1]}}) %>%
-  }
-
-  list(MultipleDupes = mult.dupes,
-       Matched = matched,
-       MissingResponse = missing.response)
+  count <- data |> group_by(across({{vars}})) %>%
+    tally() |> ungroup()
 }
 
-#a <- find_dupes(data, "ID")
+
+
+
+
+# find_dupes <- function (data, vars) { # Finding IDs with no matches
+#   count <- data %>% group_by(across({{vars}})) %>%
+#     tally() %>% ungroup() # Getting number of rows for each ID
+#   if(length(vars) == 1 ) {
+#     mult.dupes <- count %>% filter(n > 2) # If you only group by ID
+#     matched <- count %>% filter(n == 2)
+#     missing.response <- count %>% filter(n == 1)
+#   }
+#   else if(length(vars == 2)) {
+#     mult.dupes <- count %>% filter(n > 1) # If you group by ID and (pre/post)
+#     #matched <- count %>% group_by({{vars[1]}}) %>%
+#   }
+#
+#   list(MultipleDupes = mult.dupes,
+#        Matched = matched,
+#        MissingResponse = missing.response)
+# }
+#
+# a <- find_dupes(data, "ID")
+#
+# a$MultipleDupes
+# a$Matched
+# a$MissingResponse
+#
+# b <- find_dupes(data, c("ID", "group"))
+#
+# count <- data %>% group_by(ID) %>%
+#   tally()
+# count
