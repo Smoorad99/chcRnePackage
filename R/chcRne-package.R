05843e304142749a49394bb14e214ae65e9c22e9
Saul <slmooradian@csuchico.edu>
2024-09-26 13:37:17 -0700

    functioning plot option for nperc_mata

--- R/chcRne-package.R
+++ R/chcRne-package.R
@@ -4,3 +4,5 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
--- R/nperc_tbl_MATA.R
+++ R/nperc_tbl_MATA.R
@@ -3,54 +3,58 @@
 #' `nperc_tbl_MATA` can be used to create a table showing the n counts and percentages for each option in a mark all that apply question.
 #'
 #' @param df A data frame used to create the table.
-#' @param vars A character vector of variable names to include in the table (all vars have to be binary).
+#' @param vars A vector of variable names to include in the table (all vars have to be dichotomus).
+#' Input the desired variables as if you were selecting variables using dplyr's `select()` function.
 #' @param value The value in the rows of `vars` (only one value across all variables).
 #' @param row.names The name of each row in the table. Should be equivalent to each option in the select all that apply question.
 #' @param punc The punctuation at the end of the row names.
 #' @param plot If TRUE, returns a count and percent bar plot instead of a data frame. Defaults to FALSE.
 #'
 #' @return A frequency table
+#' @import ggplot2 tidyr
 #' @export
 #'
 #' @examples
 #' # Save the variable names to questions and the names of the questions to rnames
-#' cols <- c("q14_1", "q14_3", "q14_4")
 #'
 #' nperc_tbl_MATA(df = bns2_pkg_data,
-#'                vars = cols,
+#'                vars = c(q14_1:q14_4),
 #'                value = "Yes",
 #'                row.names = c("I am a broke college student",
 #'                              "No good restaurants",
 #'                              "I like cooking"),
-#'                punc = ".")
+#'                punc = ".", plot = TRUE)
 #'
 #'
 nperc_tbl_MATA <- function(df, vars, value, row.names, punc, plot = FALSE) {
-  tm <- df |> select(all_of(vars))
-  non.na <- tm |> summarise_at(vars(vars), ~sum(!is.na(.))) |> tidyr::pivot_longer(everything())
-  tm1 <- tm |> summarise_at(vars(vars), ~sum(. == value, na.rm = TRUE)) |>
-    tidyr::pivot_longer(everything())
+  tm <- df |> select({{vars}}) # Selects the vars the user inputs
+  non.na <- tm |> summarise(across({{vars}}, ~sum(!is.na(.)))) |> tidyr::pivot_longer(everything()) # get number of non na responses in each col
+  tm1 <- tm |> summarise(across({{vars}}, ~sum(. == value, na.rm = TRUE))) |>
+    tidyr::pivot_longer(everything()) |>
+    arrange(desc(value)) |>
+    rename("Question" = "name", "Count" = "value") # sum count of responses  in each col, pivot, put in descending order, and rename cols.
   tm2 <- tm1 |> mutate(
     n = non.na$value,
-    npct = paste0(value, " (", unname(formattable::percent(value/n, digits = 1)), ")"),
+    npct = paste0(Count, " (", unname(formattable::percent(Count/n, digits = 1)), ")"),
     col = paste0(row.names, punc, " (n = ", n, ")")
-  )
-  tm3 <- tm2 |> select(col, npct)
+  ) # Add columns for non na responses, and format count and percent
+  tm3 <- tm2 |> select(col, npct) # clean up
+  bp <- tm2 |> ggplot(aes(x = Question, y = Count)) +
+    geom_col() + geom_text(aes(label = npct), vjust = -0.2)
   if (plot) {
-    ggplot2::ggplot(tm2, ggplot2::aes(x = name, y = value)) +
-      ggplot2::geom_col() + ggplot2::geom_text(ggplot2::aes(label = npct), vjust = -0.2)
+    return(bp)
   }
   if (!plot) {
-    tm3
+    return(tm3)
   }
 }
 
 ## Testing ---
-df <- bns2_pkg_data
-vars <- c("q14_1", "q14_3", "q14_4")
-value <- "Yes"
-row.names <- c("I am a broke college student","No good restaurants", "I like cooking")
-punc <- ""
+# df <- bns2_pkg_data
+# vars <- "q14_1"
+# value <- "Yes"
+# row.names <- c("I am a broke college student")
+# punc <- ""
 
 
 
@@ -91,4 +95,4 @@ punc <- ""
 # Rework for future release (not the next one)
 # Option to show as plot
 # If plot = TRUE.. take pre-summarized data call ggplot and use geom_column
-
+x <- list()
